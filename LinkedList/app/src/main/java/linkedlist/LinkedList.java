cda0b92e3e40e0982b2a5dab452dee7fc8d84201
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-08-17 20:26:03 +0300

    singlyList

--- LinkedList/app/src/main/java/linkedlist/LinkedList.java
+++ LinkedList/app/src/main/java/linkedlist/LinkedList.java
@@ -34,4 +34,17 @@ public class LinkedList {
         result.append("NULL");
         return result.toString();
     }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insert(3);
+        linkedList.insert(2);
+        linkedList.insert(1);
+
+        System.out.println("Linked List: " + linkedList.toString());
+
+        System.out.println("Includes 2: " + linkedList.includes(2));
+        System.out.println("Includes 50: " + linkedList.includes(50));
+    }
 }
